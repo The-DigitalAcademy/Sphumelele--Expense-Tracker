d325176630c3e162d8d25bb31ea0b155829c1b1d
e <sphumelele762>
2022-07-22 15:36:05 +0200

    table

--- styles.js
+++ styles.js
@@ -1,103 +1,52 @@
-const balance = document.getElementById('balance')
-const money_plus = document.getElementById('money-plus')
-const money_minus = document.getElementById('money-minus')
-const list = document.getElementById('list')
-const form = document.getElementById('form')
-const text = document.getElementById('text')
-const amount = document.getElementById('amount')
+var balance = 5500;
+var income = 400;
+var expense = 2290;
+var history = [];
 
-const localStorageTransactions = JSON.parse(
-    localStorage.getItem('transactions')
-    );
-
-let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
-
-// Add transaction
-function addTransaction(e) {
-    e.preventDefault();
-
-    if(text.value.trim() === '' || amount.value.trim() === '') {
-        alert('Please add a text and amount');
-    }else {
-        const transaction = {
-            id: generateID(),
-            text: text.value,
-            amount: +amount.value
-        };
-
-        transactions.push(transaction);
-
-        addTransactionDOM(transaction);
-
-        updateValues()
-
-        updateLocalStorage()
-
-        text.value = ''
-        amount.value = ''
-    }
-}
-
-//generate random ID
-function generateID() {
-    return Math.floor(Math.random() * 100000000);
-}
 //Add transactions to DOM list
-function addTransactionDOM(transaction) {
-    //get sign
-    const sign = transaction.amount < 0 ? '-' : '+';
+// function addTransaction() {}
 
-    const item = document.createElement('li');
+// //update the balance, income and expense
+// function updateValues() {
+   
+//     text = document.getElementById('text');
+//      amount = Number (document.getElementById('amount').value);
+// }
 
-    // Add class based on value
-    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
-    item.innerHTML = `
+function displayS() {
+    var balanceE = document.getElementById('balancetotal')
+    var moneyIncome = document.getElementById('moneyplus')
+    var moneyExpense = document.getElementById('moneyminus')
+
+    tabledata = document.getElementById('tabledata')
+
+    tabledata.innerHTML = `<table">
+    <tr>
+      <th>Company</th>
+      <th>Contact</th>
+      <th>Country</th>
+    </tr>
     
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
-        <button class="delete-btn" onclick="removeTransaction(${transaction.id}) ">x</button>
-    `;
-    list.appendChild(item);
+</table>`
+
+for (let index = 0; index < history.length; index++) {
+    tabledata.innerHTML  += `<table>
+    <tr>
+      <td>fgbbnhf</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+
+    </table>`
+    
 }
-
-//update the balance, income and expense
-function updateValues() {
-    const amounts = transactions.map(transaction => transaction.amount);
-
-    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-
-    const income = amounts
-        .filter(item => item > 0)
-        .reduce((acc, item) => (acc += item), 0)
-        .toFixed(2);
-
-    const expense = (
-        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
-
-    balance.innerText = `R${total}`;
-    money_plus.innerText = `R${income}`;
-    money_minus.innerText = `R${expense}`;
-}
-
-//Remove transaction by ID
-function removeTransaction(id) {
-    transactions = transactions.filter(transaction => transaction.id !== id);
-
-    updateLocalStorage();
-
-    init();
-
-}
-//update local storage transactions
-function updateLocalStorage() {
-    localStorage.setItem('transactions', JSON.stringify(transactions));
-}
-//Init app
-function init() {
-    list.innerHTML = '';
-
-    transactions.forEach(addTransactionDOM);
-    updateValues()
-}
-
-init();
-form.addEventListener('submit', addTransaction);
+    balancetotal.innerHTML = `R${balance}`;
+    moneyplus.innerHTML = `R${income}`;
+    moneyminus.innerHTML = `R${expense}`;
+}   
+displayS();
