ca7b532c1d11425750517b0989c140acc6348472
e <sphumelele762>
2022-08-05 15:57:42 +0200

    fix jss

--- styles.js
+++ styles.js
@@ -1,52 +1,94 @@
-var balance = 5500;
-var income = 400;
-var expense = 2290;
-var history = [];
+const balance = document.getElementById('balance');
+const minus = document.getElementById('money-minus');
+const plus = document.getElementById('money-plus');
+const history = document.getElementById('list');
+const newTrans = document.getElementById('form');
+const expense = document.getElementById('text');
+const amount = document.getElementById('amount');
 
-//Add transactions to DOM list
-// function addTransaction() {}
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'))
 
-// //update the balance, income and expense
-// function updateValues() {
-   
-//     text = document.getElementById('text');
-//      amount = Number (document.getElementById('amount').value);
-// }
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
-function displayS() {
-    var balanceE = document.getElementById('balancetotal')
-    var moneyIncome = document.getElementById('moneyplus')
-    var moneyExpense = document.getElementById('moneyminus')
+function addTransaction(e) {
+    e.preventDefault();
 
-    tabledata = document.getElementById('tabledata')
+    if(expense.value.trim() === '' || amount.value.trim() === '') {
+        return;
+    }else {
+        const transaction = {
+            id: generateID(),
+            expense: expense.value,
+            amount: +amount.value
+        }
 
-    tabledata.innerHTML = `<table">
-    <tr>
-      <th>Company</th>
-      <th>Contact</th>
-      <th>Country</th>
-    </tr>
-    
-</table>`
+        transactions.push(transaction);
+        updateLocalStorage();
+        addTransactionsDOM(transaction);
+      
+        expense.value = '';
+        amount.value = '';
+        updateValues();
+    }
 
-for (let index = 0; index < history.length; index++) {
-    tabledata.innerHTML  += `<table>
-    <tr>
-      <td>fgbbnhf</td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
+} 
 
-    </table>`
-    
+function generateID() {
+    return Math.floor(Math.random() * 10000)
 }
-    balancetotal.innerHTML = `R${balance}`;
-    moneyplus.innerHTML = `R${income}`;
-    moneyminus.innerHTML = `R${expense}`;
-}   
-displayS();
+
+function addTransactionsDOM(transaction) {
+    const sign = transaction.amount < 0 ? '-' : '+' ;
+    const item = document.createElement('ol');
+
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+    item.innerHTML = `${transaction.expense} <span> R${Math.abs(transaction.amount)} </span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>
+    `
+    history.appendChild(item);
+}
+
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    init();
+    updateValues();
+    updateLocalStorage();
+}
+
+function updateValues() {
+    const amounts = transactions.map(transaction => 
+        transaction.amount)
+
+     const total = amounts.reduce((a, b) => {
+            return a + b;
+        },0).toFixed(2);
+
+       const income = amounts.filter(item => item > 0 )
+                              .reduce((acc,item) => (acc += item),0)
+                              .toFixed(2);
+
+       const expense = (amounts.filter(item => item < 0)
+                               .reduce((acc, item) => (acc += item),0) * -1.).toFixed(2)
+                               
+
+        balance.innerText = `R ${total}`
+        plus.innerText = ` R${income}`
+        minus.innerText = `R${expense}`
+    }; 
+
+    function updateLocalStorage() {
+        localStorage.setItem('transactions', JSON.stringify(transactions))
+    }
+
+function init() {
+    history.innerHTML = '';
+    transactions.forEach(addTransactionsDOM)
+}
+
+init();
+updateLocalStorage();
+updateValues();
+
+
+newTrans.addEventListener('submit', addTransaction)
